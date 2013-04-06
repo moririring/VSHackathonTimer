1e7be8b90012d09a3ee2f0d212c5b9107226349a
moririring <moriri.ring10@gmail.com>
2013-04-07 00:27:55 +0900

    VSHackathonTimer Making

---
+++ VSHackathonTimerTest/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VSHackathonTimerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //[TestCleanup]
+        VSCountDown cd = new VSCountDown();
+
+        [TestMethod]
+        public void カウントダウンタイマーから時間取得()
+        {
+            Assert.IsTrue(cd.IntTime == 0);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの３桁目を減少()
+        {
+            cd.SetTimer(VSCountDown.UpDown.Down, VSCountDown.Digit.Digit_m01);
+            Assert.IsTrue(cd.IntTime == 900);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの2桁目を増加()
+        {
+            cd.SetTimer(VSCountDown.UpDown.Up, VSCountDown.Digit.Digit_s10);
+            Assert.IsTrue(cd.IntTime == 10);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの2桁目を増加した時間()
+        {
+            cd.SetTimer(VSCountDown.UpDown.Up, VSCountDown.Digit.Digit_s10);
+            Assert.IsTrue(cd.DateTimeTime.ToLongTimeString() == "0:00:10");
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの4と5桁目を増加()
+        {
+            cd.SetTimer(VSCountDown.UpDown.Up, VSCountDown.Digit.Digit_h01);
+            cd.SetTimer(VSCountDown.UpDown.Up, VSCountDown.Digit.Digit_h10);
+            Assert.IsTrue(cd.IntTime == 110000);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの4と5桁目を増加した時間()
+        {
+            cd.SetTimer(VSCountDown.UpDown.Up, VSCountDown.Digit.Digit_h01);
+            cd.SetTimer(VSCountDown.UpDown.Up, VSCountDown.Digit.Digit_h10);
+            Assert.IsTrue(cd.DateTimeTime.ToLongTimeString() == "11:00:00");
+        }
+    }
+}
---
+++ VSHackathonTimerTest/VSCountDown.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VSHackathonTimerTest
+{
+    class VSCountDown
+    {
+        public enum UpDown
+        {
+            Up,
+            Down,
+        };
+        public enum Digit
+        {
+            Digit_s01,
+            Digit_s10,
+            Digit_m01,
+            Digit_m10,
+            Digit_h01,
+            Digit_h10,
+            Digit_Max,
+        };
+        public int[] timer = new int[] { 0, 0, 0, 0, 0, 0 };
+        public int[] Timer
+        {
+            get { return timer; }
+        }
+
+        public int IntTime { private set; get; }               
+        public DateTime DateTimeTime { private set; get; }
+
+        private void CalcTime()
+        {
+            IntTime = 0;
+            int dig = 1;
+            foreach (var t in timer)
+            {
+                IntTime += t * dig;
+                dig *= 10;
+            }
+            DateTimeTime = new DateTime(0);
+            DateTimeTime = DateTimeTime.AddSeconds(timer[(int)Digit.Digit_s10] * 10 + timer[(int)Digit.Digit_s01] * 1);
+            DateTimeTime = DateTimeTime.AddMinutes(timer[(int)Digit.Digit_m10] * 10 + timer[(int)Digit.Digit_m01] * 1);
+            DateTimeTime = DateTimeTime.AddHours(timer[(int)Digit.Digit_h10] * 10 + timer[(int)Digit.Digit_h01] * 1);
+        }
+        public void SetTimer(UpDown ud, Digit p)
+        {
+            if (ud == UpDown.Up)
+            {
+                timer[(int)p] = timer[(int)p] + 1 % 10;
+            }
+            else
+            {
+                timer[(int)p] = timer[(int)p] + 9 % 10;
+            }
+            CalcTime();
+        }
+    }
+}
