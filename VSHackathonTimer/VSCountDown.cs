55885a1a81a6f58d588c0f228cc68c17e29f9a94
moririring <moriri.ring10@gmailcom>
2013-04-08 20:16:24 +0900

    Test Write

--- VSHackathonTimer/VSCountDown.cs
+++ VSHackathonTimer/VSCountDown.cs
@@ -7,58 +7,70 @@ namespace VSHackathonTimer
 {
     public class VSCountDown
     {
-        class VSCountDown
+        public enum PlusMinus
         {
-            public enum PlusMinus
-            {
-                Plus,
-                Minus,
-            };
-            public enum Digit
-            {
-                Digit_s01,
-                Digit_s10,
-                Digit_m01,
-                Digit_m10,
-                Digit_h01,
-                Digit_h10,
-                Digit_Max,
-            };
-            public int[] timer = new int[] { 0, 0, 0, 0, 0, 0 };
-            public int[] Timer
-            {
-                get { return timer; }
-            }
-
-            public int IntTime { private set; get; }
-            public DateTime DateTimeTime { private set; get; }
-
-            private void CalcTime()
-            {
-                IntTime = 0;
-                int dig = 1;
-                foreach (var t in timer)
-                {
-                    IntTime += t * dig;
-                    dig *= 10;
-                }
-                DateTimeTime = new DateTime(0);
-                DateTimeTime = DateTimeTime.AddSeconds(timer[(int)Digit.Digit_s10] * 10 + timer[(int)Digit.Digit_s01] * 1);
-                DateTimeTime = DateTimeTime.AddMinutes(timer[(int)Digit.Digit_m10] * 10 + timer[(int)Digit.Digit_m01] * 1);
-                DateTimeTime = DateTimeTime.AddHours(timer[(int)Digit.Digit_h10] * 10 + timer[(int)Digit.Digit_h01] * 1);
-            }
-            public void SetTimer(PlusMinus ud, Digit p)
-            {
-                if (ud == PlusMinus.Plus)
-                {
-                    timer[(int)p] = timer[(int)p] + 1 % 10;
-                }
-                else
-                {
-                    timer[(int)p] = timer[(int)p] + 9 % 10;
-                }
-                CalcTime();
-            }
+            Plus,
+            Minus,
+        };
+        public enum Digit
+        {
+            Digit_s01,
+            Digit_s10,
+            Digit_m01,
+            Digit_m10,
+            Digit_h01,
+            Digit_h10,
+            Digit_Max,
+        };
+        public int[] timer = new int[] { 0, 0, 0, 0, 0, 0 };
+        public int[] Timer
+        {
+            get { return timer; }
         }
+
+        public int IntTime { private set; get; }
+        public string StringTime { private set; get; }
+        public DateTime DateTimeTime { private set; get; }
+
+        private void CalcTime()
+        {
+            IntTime = 0;
+            int dig = 1;
+            foreach (var t in timer)
+            {
+                IntTime += t * dig;
+                dig *= 10;
+            }
+            int dotCount = 0;
+            もう少しリファクタリング
+            var st = new StringBuilder();
+            foreach (var t in timer.Reverse())
+            {
+                if (dotCount % 2 == 0 && dotCount != 0)
+                {
+                    st.Append(":");
+                }
+                st.Append(t);
+                dotCount++;
+            }
+            StringTime = st.ToString();
+            DateTimeTime = new DateTime(0);
+            DateTimeTime = DateTimeTime.AddSeconds(timer[(int)Digit.Digit_s10] * 10 + timer[(int)Digit.Digit_s01] * 1);
+            DateTimeTime = DateTimeTime.AddMinutes(timer[(int)Digit.Digit_m10] * 10 + timer[(int)Digit.Digit_m01] * 1);
+            DateTimeTime = DateTimeTime.AddHours(timer[(int)Digit.Digit_h10] * 10 + timer[(int)Digit.Digit_h01] * 1);
+        }
+        public void SetTimer(PlusMinus ud, Digit p)
+        {
+            if (ud == PlusMinus.Plus)
+            {
+                timer[(int)p] = (timer[(int)p] + 1) % 10;
+            }
+            else
+            {
+                timer[(int)p] = (timer[(int)p] + (10 - 1)) % 10;
+            }
+            CalcTime();
+        }
+
     }
 }
--- VSHackathonTimerTest/UnitTest1.cs
+++ VSHackathonTimerTest/UnitTest1.cs
@@ -10,11 +10,58 @@ namespace VSHackathonTimerTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            VSCountDown cd = new VSCountDown();
+        VSCountDown cd = new VSCountDown();
 
+        [TestMethod]
+        public void カウントダウンタイマーから時間取得()
+        {
+            Assert.IsTrue(cd.IntTime == 0);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの３桁目を減少()
+        {
+            cd.SetTimer(VSCountDown.PlusMinus.Minus, VSCountDown.Digit.Digit_m01);
+            Assert.IsTrue(cd.IntTime == 900);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの3桁目2回減少()
+        {
+            cd.SetTimer(VSCountDown.PlusMinus.Minus, VSCountDown.Digit.Digit_m01);
+            cd.SetTimer(VSCountDown.PlusMinus.Minus, VSCountDown.Digit.Digit_m01);
+            Assert.IsTrue(cd.IntTime == 800);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの2桁目を増加()
+        {
+            cd.SetTimer(VSCountDown.PlusMinus.Plus, VSCountDown.Digit.Digit_s10);
+            Assert.IsTrue(cd.IntTime == 10);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの2桁目を増加した時間()
+        {
+            cd.SetTimer(VSCountDown.PlusMinus.Plus, VSCountDown.Digit.Digit_s10);
+            Assert.IsTrue(cd.DateTimeTime.ToString("HH:mm:ss") == "00:00:10");
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの4と5桁目を増加()
+        {
+            cd.SetTimer(VSCountDown.PlusMinus.Plus, VSCountDown.Digit.Digit_h01);
+            cd.SetTimer(VSCountDown.PlusMinus.Plus, VSCountDown.Digit.Digit_h10);
+            Assert.IsTrue(cd.IntTime == 110000);
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの4と5桁目を増加した時間()
+        {
+            cd.SetTimer(VSCountDown.PlusMinus.Plus, VSCountDown.Digit.Digit_h01);
+            cd.SetTimer(VSCountDown.PlusMinus.Plus, VSCountDown.Digit.Digit_h10);
+            Assert.IsTrue(cd.DateTimeTime.ToString("HH:mm:ss") == "11:00:00");
+        }
+        [TestMethod]
+        public void カウントダウンタイマーの99分をチェック()
+        {
+            cd.SetTimer(VSCountDown.PlusMinus.Minus, VSCountDown.Digit.Digit_m01);
+            cd.SetTimer(VSCountDown.PlusMinus.Minus, VSCountDown.Digit.Digit_m10);
+            Assert.IsTrue(cd.StringTime == "00:99:00");
         }
     }
 }
